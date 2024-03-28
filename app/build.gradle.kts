157cf4947e6eaa92ceff20e56eb467de92d78c31
MichaleBob <manhdrahx354@gmail.com>
2024-03-28 14:54:10 +0700

    reset2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.github.bumptech.glide:glide:5.0.0-rc01")
+    implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -50,4 +52,9 @@ dependencies {
     implementation("com.airbnb.android:lottie:6.4.0")
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("com.airbnb.android:lottie:6.4.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+
 }
--- app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.myapplication.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.myapplication.Adapter.PopularAdapter;
+import com.example.myapplication.Display.IpadDisplay;
+import com.example.myapplication.Display.IphoneDisplay;
+import com.example.myapplication.Display.MacDisplay;
+import com.example.myapplication.Display.WatchDisplay;
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.ActivityMainBinding;
 import com.example.myapplication.domain.PopularDomain;
@@ -21,7 +28,47 @@ ActivityMainBinding binding;
         setContentView(binding.getRoot());
 
         initRecycleView();
-        
+
+        //chuyển sang các layout Display iph,ip,...
+                ImageView im_iph, im_mac, im_ip, im_watch;
+
+        im_iph = findViewById(R.id.dis_ip);
+        im_mac = findViewById(R.id.dis_mac);
+        im_ip = findViewById(R.id.dis_ipad);
+        im_watch = findViewById(R.id.dis_watch);
+
+        im_iph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, IphoneDisplay.class);
+                startActivity(intent);
+            }
+        });
+
+        im_mac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MacDisplay.class);
+                startActivity(intent);
+            }
+        });
+
+        im_ip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, IpadDisplay.class);
+                startActivity(intent);
+            }
+        });
+
+        im_watch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WatchDisplay.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     private void initRecycleView() {
