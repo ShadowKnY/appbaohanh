e2ed67c0768595b31b9f5a70ce8266122f9a5c77
ShadowKnY <minhm5993@gmail.com>
2024-04-07 21:57:32 +0700

    đã đổ xong dữ liệu iPhone vào recycle view

--- app/src/main/java/com/example/myapplication/Display/IpadDisplay.java
+++ app/src/main/java/com/example/myapplication/Display/IpadDisplay.java
@@ -1,53 +1,90 @@
 package com.example.myapplication.Display;
 
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-
-import com.example.myapplication.Adapter.DisplayAdapter;
-import com.example.myapplication.Adapter.PopularAdapter;
+import com.example.myapplication.Adapter.displayAdapter;
 import com.example.myapplication.R;
-import com.example.myapplication.databinding.ActivityMainBinding;
-import com.example.myapplication.databinding.IpadDisplayBinding;
 import com.example.myapplication.domain.PopularDomain;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class IpadDisplay extends AppCompatActivity {
-    IpadDisplayBinding binding;
-
+    private EditText edtTitle,edtPrice,edtScr,edtReview;
+    private RecyclerView rcvProduct;
+    private displayAdapter mdisplayAdapter;
+    private List<PopularDomain> mlistProduct;
+    private String category;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = IpadDisplayBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_display);
 
-        initRecyclerView();
+        initUI();
+
+//        Intent intent = getIntent();
+//        if (intent != null) {
+//            category = intent.getStringExtra("category");
+//            // Kiểm tra xem category có giá trị hay không
+//            if (category != null && !category.isEmpty()) {
+//                // Gọi phương thức để lấy danh sách sản phẩm từ Firebase dựa trên category
+//                getListFromRealTimeDb(category);
+//            }
+//        }
+
+
+        getListFromRealTimeDb();
+
+
+    }
+    private void initUI(){
+//        edtTitle = findViewById(R.id.titleTxt);
+//        edtPrice = findViewById(R.id.feeTxt);
+        rcvProduct = findViewById(R.id.ipadView);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2);
+        rcvProduct.setLayoutManager(gridLayoutManager);
+
+//        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,DividerItemDecoration.HORIZONTAL);
+//        rcvProduct.addItemDecoration(dividerItemDecoration);
+        mlistProduct = new ArrayList<>();
+        mdisplayAdapter = new displayAdapter(mlistProduct);
+        rcvProduct.setAdapter(mdisplayAdapter);
+    }
+    private void getListFromRealTimeDb(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myProd = database.getReference("Product").child("iPhone");
+        myProd.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mlistProduct.clear();
+                for (DataSnapshot productSnapshot : snapshot.getChildren()) {
+                    // Lặp qua tất cả các sản phẩm trong danh mục "iPhone"
+                    for (DataSnapshot itemSnapshot : productSnapshot.getChildren()) {
+                        PopularDomain product = itemSnapshot.getValue(PopularDomain.class);
+                        mlistProduct.add(product);
+                    }
+                }
+                mdisplayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(IpadDisplay.this, "Không thể lấy dữ liệu từ cơ sở dữ liệu", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    private void initRecyclerView() {
-        ArrayList<PopularDomain> items = new ArrayList<>();
-        items.add(new PopularDomain("iPad Pro M1","item_3",50,4.8,15,899,""));
-        items.add(new PopularDomain("Apple Watch Ultra","cat4",24,4,25,699,""));
-        items.add(new PopularDomain("iPad Pro 2023","cat3",11,3,26,799,""));
-        items.add(new PopularDomain("MacBook Pro 16'","item_4",16,4.3,16,1999,""));
-        items.add(new PopularDomain("iPad Pro M1","item_3",50,4.8,15,899,""));
-        items.add(new PopularDomain("Apple Watch Ultra","cat4",24,4,25,699,""));
-        items.add(new PopularDomain("iPad Pro 2023","cat3",11,3,26,799,""));
-        items.add(new PopularDomain("MacBook Pro 16'","item_4",16,4.3,16,1999,""));
-        items.add(new PopularDomain("iPad Pro M1","item_3",50,4.8,15,899,""));
-        items.add(new PopularDomain("Apple Watch Ultra","cat4",24,4,25,699,""));
-        items.add(new PopularDomain("iPad Pro 2023","cat3",11,3,26,799,""));
-        items.add(new PopularDomain("MacBook Pro 16'","item_4",16,4.3,16,1999,""));
 
-        // Sử dụng GridLayoutManager với spanCount = 2 (hai sản phẩm trên một hàng)
-        GridLayoutManager layoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
-        binding.ipadView.setLayoutManager(layoutManager);
-
-        binding.ipadView.setAdapter(new PopularAdapter(items));
-    }
 }
