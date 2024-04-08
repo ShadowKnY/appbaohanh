f4a5aa149001e0497c583131a24593a1178286f1
ShadowKnY <minhm5993@gmail.com>
2024-04-09 00:28:39 +0700

    đã kéo login của Lịch về và chạy thành công

--- app/src/main/java/com/example/myapplication/login/lich_loginform.java
+++ app/src/main/java/com/example/myapplication/login/lich_loginform.java
@@ -14,13 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.Activity.MainActivity;
 import com.example.myapplication.R;
-import com.example.myapplication.SplashScreen;
-import com.example.myapplication.add_edit_delete.addActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
--- app/src/main/java/com/example/myapplication/login/lich_registerform.java
+++ app/src/main/java/com/example/myapplication/login/lich_registerform.java
@@ -12,14 +12,11 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.myapplication.Activity.MainActivity;
 import com.example.myapplication.R;
-import com.example.myapplication.SplashScreen;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
